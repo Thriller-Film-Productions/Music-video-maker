a9f2c6876f27923eb0d1f990e5498a45249d5f7d
F4Tornado <h.rovnyak@gmail.com>
2018-10-11 20:21:04 -0400

    Autodancing!

--- upload.js
+++ upload.js
@@ -1,8 +1,21 @@
 function fileSelected(file) {
+  seq = sequencer.value().split("\n");
+  let seq2 = [];
+  for (let thing of seq) {
+    seq2.push(JSON.parse("[" + thing + "]"));
+  }
+  seq = seq2;
   removeElements();
   sound = loadSound(file, () => {
     wave = waveform(sound);
     sound.play();
+    setTimeout(() => {
+      setInterval(() => {
+        change++;
+        change %= seq.length;
+        console.log(change)
+      }, 60 / bpm.value() * 1000)
+    }, delay.value())
   })
 }
 
